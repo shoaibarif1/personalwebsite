32bae171efd1bbc1292458c0f0d74748e2b0ff1c
Shoaib Arif <94898291+shoaibarif1@users.noreply.github.com>
2023-05-08 23:05:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Add your JavaScript code here
+    // Example: Manipulating the DOM
+    var heading = document.querySelector('h1');
+    heading.textContent = 'Welcome to My Website';
+});
